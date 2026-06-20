d7c5141f374fc5609381985692c3aad0a6e05f0a
Maxim Bodnar <merg1.sharex@gmail.com>
2026-06-21 01:17:53 +0300

    Edit hyprland conf. Add alacritty, fastfetch conf.

--- dot_config/hypr/configs/env.lua
+++ dot_config/hypr/configs/env.lua
@@ -19,5 +19,5 @@ hl.env("__GLX_VENDOR_LIBRARY_NAME", "nvidia")
 --------------
 
 hl.env("QT_QPA_PLATFORMTHEME", "qt6ct")
-hl.env("GTK_THEME", "Breeze-Dark")
-hl.env("GTK_ICON_THEME", "breeze-dark")
+hl.env("GTK_THEME", "Breeze:dark")
+hl.env("GTK_ICON_THEME", "Papirus-Dark")
--- dot_config/hypr/configs/exec.lua
+++ dot_config/hypr/configs/exec.lua
@@ -1,8 +1,18 @@
-hl.on("hyprland.start", function ()
-  hl.exec_cmd("systemctl --user start hyprpolkitagent")
-  hl.exec_cmd("hyprpaper")
-  hl.exec_cmd("waybar")
-  hl.exec_cmd("kanshi")
-  hl.exec_cmd("dunst")
-  hl.exec_cmd("udiskie -t")
+hl.on("hyprland.start", function()
+	hl.exec_cmd("systemctl --user start hyprpolkitagent")
+	hl.exec_cmd("hyprpaper")
+	hl.exec_cmd("waybar")
+	hl.exec_cmd("kanshi")
+	hl.exec_cmd("dunst")
+	hl.exec_cmd("udiskie -t")
+	hl.exec_cmd("gsettings set org.gnome.desktop.interface icon-theme Papirus-Dark")
+	---FIX OBS---
+	hl.exec_cmd("killall -9 xdg-desktop-portal-hyprland xdg-desktop-portal")
+	hl.exec_cmd("dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=Hyprland")
+	hl.exec_cmd(
+		"systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
+	)
+	hl.exec_cmd("/usr/lib/xdg-desktop-portal-hyprland &")
+	hl.exec_cmd("sleep 1 && /usr/lib/xdg-desktop-portal &")
+	-------------
 end)
