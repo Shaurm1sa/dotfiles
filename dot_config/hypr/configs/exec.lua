26b0c405f64a3698c237cff7c77bc3e666cc9e01
Maxim Bodnar <merg1.sharex@gmail.com>
2026-08-15 22:50:41 +0300

    Update files. Add monitor.lua

--- dot_config/hypr/configs/exec.lua
+++ dot_config/hypr/configs/exec.lua
@@ -1,6 +1,6 @@
 hl.on("hyprland.start", function()
-	hl.exec_cmd("gsettings set org.gnome.desktop.interface icon-theme Papirus-Dark")
 	hl.exec_cmd("dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP")
+	hl.exec_cmd("systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP")
 	hl.exec_cmd("systemctl --user start hyprpolkitagent")
 	hl.exec_cmd("hyprpaper")
 	hl.exec_cmd("waybar")
---
+++ dot_config/hypr/configs/monitors.lua
@@ -0,0 +1,15 @@
+-- AOC 24G4ZR XL0RBHA002289
+-- hl.monitor({
+-- 	output = "DP-1",
+-- 	mode = "1920x1080@240",
+-- 	position = "0x0",
+-- 	scale = "1",
+-- })
+
+
+hl.monitor({
+	output = "",
+	mode = "highrr",
+	position = "auto",
+	scale = "1",
+})
